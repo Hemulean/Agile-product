7863693749dce8e07be9e48c7e7888263f675030
Emil Nilsson <emilwald94@gmail.com>
2022-05-30 11:28:49 +0200

    .

--- Webshop/Program.cs
+++ Webshop/Program.cs
@@ -4,7 +4,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Product}/{action=index}");
 app.UseStaticFiles();
 app.UseRouting();
 
