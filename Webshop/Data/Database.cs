7f5de4872d495999d22159c752f3decc5a901640
Jessica Söderback <jessica.soderback@utb.ecutbildning.se>
2022-05-30 10:39:44 +0200

    Fixat det sista i databasen så att allt är sorterat och ifyllt med värden, det finns inga null/0-värden kvar från APIet. (Jessica & Sofiia)

--- Webshop/Data/Database.cs
+++ Webshop/Data/Database.cs
@@ -34,36 +34,72 @@ namespace Webshop.Data
 
         public async Task Seed()
         {
-          
+
             var responseList = await _apiHandler.GetAllDataFromApi();
 
             foreach (var item in responseList)
             {
+
                 //TODO price (random-generator), category, 
                 if (item.Category != null & item.Category != "")
                 {
+                    Random rnd = new Random();
 
-                    if (item.Price == "0.0" | item.Price == null) 
+                    double rating;
+                    int ratingInt;
+
+
+                    if (item.Price == "0.0" | item.Price == null)
                     {
-                        Random rnd = new Random();
+
                         var priceInt = rnd.Next(1, 35);
+                        ratingInt = rnd.Next(1, 5);
+
+                        if (item.Rating == null)
+                        {
+                            rating = Convert.ToDouble(ratingInt);
+                        }
+                        else
+                        {
+                            rating = Convert.ToDouble(item.Rating);
+                        }
 
                         var price = priceInt.ToString();
 
-                         var productRandPrice = new Product
-                         {
-                             Name = item.Name,
-                             Brand = item.Brand,
-                             Description = item.Description,
-                             Type = item.Type,
-                             Price = price,
-                             Category = item.Category,
-                             Rating = item.Rating,
-                             Image = item.Image
-                         };
+                        var productRandPrice = new Product
+                        {
+                            Name = item.Name,
+                            Brand = item.Brand,
+                            Description = item.Description,
+                            Type = item.Type,
+                            Price = price,
+                            Category = item.Category,
+                            Rating = rating,
+                            Image = item.Image
+                        };
 
-                         _ctx.Products.Add(productRandPrice);
+                        _ctx.Products.Add(productRandPrice);
 
+
+                    }
+                    else if (item.Rating == null)
+                    {
+                        ratingInt = rnd.Next(1, 5);
+                        rating = Convert.ToDouble(ratingInt);
+
+                        var product = new Product
+                        {
+                            Name = item.Name,
+                            Brand = item.Brand,
+                            Description = item.Description,
+                            Type = item.Type,
+                            Price = item.Price,
+                            Category = item.Category,
+                            Rating = rating,
+                            Image = item.Image
+                        };
+
+                        _ctx.Products.Add(product);
                     }
                     else
                     {
@@ -81,13 +117,12 @@ namespace Webshop.Data
 
                         _ctx.Products.Add(product);
                     }
-
-                   
                 }
-                
+
             }
 
             await _ctx.SaveChangesAsync();
         }
     }
 }
+  
